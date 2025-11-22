aff359758552e6371eee06fbc154eb7b6beaa974
Gevonda <GevondaYT@gmail.com>
2025-11-22 14:07:26 +0700

    Add files via upload

---
+++ loveTester.py
@@ -0,0 +1,38 @@
+import time
+import random
+
+
+print("Love Tester ∧∧")
+print("""
+   ******        ******
+ **      **    **      **
+**         ** **         **
+**          ***           **
+ **                      **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+""")
+
+
+while True:
+    Name1 = input("Enter the First Name:")
+    print("Nice Name!")
+    time.sleep(1)
+    Name2 = input("Enter the Second Name:")
+    print("Great Name!")
+    time.sleep(1)
+    print("Generating...")
+    print("=======================================")
+    time.sleep(3)
+    print("Calculating Love Percentage...")
+    print("=======================================")
+    time.sleep(2)
+    print("Seeing if names are compatible...")
+    
+    Random = random.randint(0, 100)
+    print("Result:", Random,"%")
